618cf56ddebdfc3a6af964393f3407e691c64592
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-01-12 21:54:55 +0100

    5

--- src/main/scala/Number.scala
+++ src/main/scala/Number.scala
@@ -13,10 +13,19 @@ package object Number {
 
   /** Fragen:
    * Vorteil Double gegenüber Int? Dezimalzahlen möglich.
-   * Was passiert bei x * x > [[Double.MaxValue]]? Stackoverflow sagt [[Double.MaxValue]] + 1 = [[Double.PositiveInfinity]] und kein overflow
+   * Was passiert bei x * x > [[Double.MaxValue]]? Stackoverflow sagt [[Double.MaxValue]] + 1 = [[Double.PositiveInfinity]], also kein overflow
    *
    * @param x any [[Double]]
    * @return square of x
    */
   def square(x:Double):Double = x * x;
+
+  /** Fragen:
+   * Was passiert bei max(Int.MaxValue + 1, Int.MaxValue)? Bei Int gibt es einen overflow.
+   *
+   * @param x [[Int]]
+   * @param y [[Int]]
+   * @return y if x < y, else x
+   */
+  def max(x:Int,y:Int):Int = if(x < y) y else x
 }
