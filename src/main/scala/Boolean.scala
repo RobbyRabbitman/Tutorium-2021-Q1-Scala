bfb2ba4bfae2f855dcd5ffd022e299359bb20e56
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-01-12 22:37:40 +0100

    8

--- src/main/scala/Boolean.scala
+++ src/main/scala/Boolean.scala
@@ -7,4 +7,12 @@ package object Boolean {
    * @return !(x && y)
    */
   def nand(x: Boolean, y: Boolean): Boolean = !(x && y) // !x || !y
+
+  /**
+   *
+   * @param x [[Boolean]]
+   * @param y [[Boolean]]
+   * @return x && !y || !x && y
+   */
+  def xor(x: Boolean, y: Boolean): Boolean = x && !y || !x && y
 }
