2292ad62fcfaa2ab7ac0f47f02d0116739c41bdb
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-01-13 13:14:11 +0100

    9b extra: comparision of custom modulo with built-in

--- src/main/scala/Number.scala
+++ src/main/scala/Number.scala
@@ -36,7 +36,9 @@ package object Number {
    */
   def abs(x: Int): Int = if (x < 0) -x else x // math.abs(x)
 
-  /**
+  /** Fragen: Warum kann es sinnvoll sein seine eigenen mathematischen Funktionen zu bauen?
+   * Es kann unterschiedliche Definitionen und Konventionen geben.
+   * Zum Beispiel beim Modulo, was aber hier nicht der Fall ist.
    *
    * @param n [[Int]]
    * @param m [[Int]]
@@ -46,7 +48,7 @@ package object Number {
     val abs_n: Int = abs(n)
     val abs_m: Int = abs(m)
     var r: Int = abs_n
-    if (abs_m == 0) throw new IllegalArgumentException("The divider in modulo must not be 0")
+    if (abs_m == 0) throw new ArithmeticException("The divider in modulo must not be 0")
     while (r >= abs_m) r -= abs_m
     if (n < 0) -r else r
   }
