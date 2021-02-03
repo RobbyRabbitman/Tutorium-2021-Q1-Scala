ad6ecefdf829df5baaca385ed18fb8d7d37754bc
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-02-03 15:25:09 +0100

    24

--- src/main/scala/String.scala
+++ src/main/scala/String.scala
@@ -11,4 +11,20 @@ package object String {
    * @return word reversed. abcd -> dcba
    */
   def reverse(word: String): String = if (word.length == 0) "" else concat(reverse(tail(word)), head(word).toString())
+
+  /**
+   *
+   * @param word
+   * @param searchTerm
+   * @return how often word contains searchTearm. Returns 0 if searchTearm.length == 0
+   *
+   * (HansHugo,Hans) -> 1, (aaabbb, a) -> 3
+   */
+  def containsHowOften(word: String, searchTerm: String): Int = {
+    if (word.length < searchTerm.length || searchTerm.isEmpty) return 0
+    val contains: Int = if (word.substring(0, searchTerm.length) == searchTerm) 1 else 0
+    containsHowOften(tail(word), searchTerm) + contains
+  }
+
+
 }
--- src/test/scala/String.scala
+++ src/test/scala/String.scala
@@ -10,3 +10,12 @@ class ReverseTest extends FunSuite {
   test("Expect reverse of 'abcd' to be 'dcba'")(assert(reverse("abcd") === "dcba"))
   test("Expect reverse of ' ' to be ' '")(assert(reverse(" ") === " "))
 }
+
+class ContainsHowOften extends FunSuite {
+  test("Expect aaa to contain a 3 times")(assert(containsHowOften("aaa", "a") === 3))
+  test("Expect aa a to contain a 3 times")(assert(containsHowOften("aa a", "a") === 3))
+  test("Expect '' to contain a 0 times")(assert(containsHowOften("", "a") === 0))
+  test("Expect '' to contain '' 0 times")(assert(containsHowOften("aaa", "a") === 3))
+  test("Expect '   ' to contain '   ' 3 times")(assert(containsHowOften("   ", " ") === 3))
+  test("Expect aaa to contain '' 0 times")(assert(containsHowOften("aaa", "") === 0))
+}
