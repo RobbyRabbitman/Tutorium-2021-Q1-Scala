d383c6d0633132708742d9d51c7d1d88574a6514
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-02-11 19:19:57 +0100

    31

--- src/main/scala/Boolean.scala
+++ src/main/scala/Boolean.scala
@@ -1,12 +1,12 @@
 
 package object Boolean {
 
-  def trueValue():Boolean = {
+  def trueValue(): Boolean = {
     println("return true!")
     true
   }
 
-  def falseValue():Boolean = {
+  def falseValue(): Boolean = {
     println("return false!")
     false
   }
@@ -25,5 +25,15 @@ package object Boolean {
    * @param y [[Boolean]]
    * @return x && !y || !x && y
    */
-  def xor(x: Boolean, y: Boolean): Boolean = x && !y || !x && y
+  def xor_lazy(x: Boolean, y: Boolean): Boolean = x && !y || !x && y
+
+  def xor(x: Boolean, y: Boolean): Boolean = x != y
+
+  def equal(x: Boolean, y: Boolean): Boolean = x == y
+
+  def implies(x: Boolean, y: Boolean): Boolean = !x || y
+
+  def xor_withIfs(x: Boolean, y: Boolean): Boolean = if (x) {
+    if (y) false else true
+  } else if (y) true else false
 }
--- src/test/scala/Boolean.scala
+++ src/test/scala/Boolean.scala
@@ -8,11 +8,39 @@ class NandTest extends FunSuite {
   test("Expect !(false && false) to be true")(assert(nand(false, false) === true))
 }
 
+class XorLazyTest extends FunSuite {
+  test("Expect xor_lazy(true,true) to be false")(assert(xor_lazy(true, true) === false))
+  test("Expect xor_lazy(true,false) to be true")(assert(xor_lazy(true, false) === true))
+  test("Expect xor_lazy(false,true) to be true")(assert(xor_lazy(false, true) === true))
+  test("Expect xor_lazy(false,false) to be false")(assert(xor_lazy(false, false) === false))
+}
+
 class XorTest extends FunSuite {
   test("Expect xor(true,true) to be false")(assert(xor(true, true) === false))
   test("Expect xor(true,false) to be true")(assert(xor(true, false) === true))
   test("Expect xor(false,true) to be true")(assert(xor(false, true) === true))
   test("Expect xor(false,false) to be false")(assert(xor(false, false) === false))
+}
+
+class XorWithIfsTest extends FunSuite {
+  test("Expect xor_withIfs(true,true) to be false")(assert(xor_withIfs(true, true) === false))
+  test("Expect xor_withIfs(true,false) to be true")(assert(xor_withIfs(true, false) === true))
+  test("Expect xor_withIfs(false,true) to be true")(assert(xor_withIfs(false, true) === true))
+  test("Expect xor_withIfs(false,false) to be false")(assert(xor_withIfs(false, false) === false))
+}
+
+class EqualsTest extends FunSuite {
+  test("Expect equal(true,true) to be true")(assert(equal(true, true) === true))
+  test("Expect equal(true,false) to be false")(assert(equal(true, false) === false))
+  test("Expect equal(false,true) to be false")(assert(equal(false, true) === false))
+  test("Expect equal(false,false) to be true")(assert(equal(false, false) === true))
+}
+
+class ImpliesTest extends FunSuite {
+  test("Expect implies(true,true) to be true")(assert(implies(true, true) === true))
+  test("Expect implies(true,false) to be false")(assert(implies(true, false) === false))
+  test("Expect implies(false,true) to be true")(assert(implies(false, true) === true))
+  test("Expect implies(false,false) to be true")(assert(implies(false, false) === true))
 }
 
 /**
